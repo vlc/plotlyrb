a6b490e1805331cc23cb23643d572b9317027318
Andrew McCluskey <andrew@ajmccluskey.com>
2016-10-25 11:23:53 +1000

    Add dummy grid test

---
+++ test/plotlyrb/grid_test.rb
@@ -0,0 +1,9 @@
+module Plotlyrb
+  class GridTest < Test::Unit::TestCase
+    def test_get_image
+      plotly = Plotlyrb::ApiV2.auth_plotly(USERNAME, API_KEY)
+      data = [{:x => [1,2,3], :y => [2,4,6], :type => 'scatter'}]
+      assert(true)
+    end
+  end
+end
