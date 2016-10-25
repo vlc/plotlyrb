63325bf74c9865717ee35f1e97c3ad34dfb51a5d
Andrew McCluskey <andrew@ajmccluskey.com>
2016-10-25 11:28:43 +1000

    Get assertion arguments around the right way

--- test/plotlyrb/plot_image_test.rb
+++ test/plotlyrb/plot_image_test.rb
@@ -8,7 +8,7 @@ module Plotlyrb
       data = [{:x => [1,2,3], :y => [2,4,6], :type => 'scatter'}]
       tmp_file = Tempfile.new('get_image_test').path
       plotly.plot_image(data, tmp_file, :png)
-      assert('File returned should be same', FileUtils.identical?("#{File.dirname(__FILE__)}/../fixtures/get_test.png", tmp_file))
+      assert(FileUtils.identical?("#{File.dirname(__FILE__)}/../fixtures/get_test.png", tmp_file), 'File returned should be same')
     end
   end
 end
