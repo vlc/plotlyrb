b49f1473a459d0d1e32e97917ce1ffcd4c6e0dab
Andrew McCluskey <andrew@ajmccluskey.com>
2016-11-02 15:12:46 +1000

    Add layout to test_get_image

--- test/plotlyrb/plot_image_test.rb
+++ test/plotlyrb/plot_image_test.rb
@@ -5,7 +5,12 @@ module Plotlyrb
   class PlotImageTest < Test::Unit::TestCase
     def test_get_image
       plotly = Plotlyrb::ApiV2.auth_plotly(USERNAME, API_KEY)
-      data = {:figure => {:data => [{:x => [1,2,3], :y => [2,4,6], :type => 'scatter'}]}, :format => :png}
+      data = {:figure => {
+                :data => [{:x => [1,2,3], :y => [2,4,6], :type => 'scatter'}],
+                :layout => {:xaxis => {:title => 'one-two-three'}, :yaxis => {:title => 'two-four-six'}}
+              },
+              :format => :png
+             }
       tmp_file = Tempfile.new('get_image_test').path
       plotly.plot_image(data, tmp_file)
       assert(FileUtils.identical?("#{File.dirname(__FILE__)}/../fixtures/get_test.png", tmp_file), 'File returned should be same')
